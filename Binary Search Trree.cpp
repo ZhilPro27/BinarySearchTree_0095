1e79b6e32b0425baae8c6a6330f7bcb84ede104d
ZhilPro27 <zhilalkrisna@gmail.com>
2024-06-14 13:57:13 +0700

    Membuat constructor untuk class Node

--- Binary Search Trree.cpp
+++ Binary Search Trree.cpp
@@ -7,4 +7,11 @@ public :
 	string info;
 	Node* leftchild;
 	Node* rightchild;
+
+	// Constructor for the node class
+	Node(string i, Node* l, Node* r) {
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
 };
